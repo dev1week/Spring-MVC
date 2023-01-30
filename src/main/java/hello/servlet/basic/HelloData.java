c7b0ab5121e36bf90a85214ab9e62b24f495a6a5
dev1week <rondo2860@gmail.com>
2023-01-31 02:07:14 +0900

    http 요청 데이터 정리

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,22 @@
+package hello.servlet.basic;
+
+public class HelloData {
+    private String userName;
+    private int age;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
